b6b5b784a12541e697174065c1fb182da42f5d31
Tony Sprocket <tonysprocket@gmail.com>
2025-04-02 14:19:31 -0600

    set up vite + typescript + three template

---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite'
+import path from 'path'
+
+export default defineConfig({
+  base: './', // Use relative paths (important if you're deploying somewhere like file:// or subdirectories)
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    sourcemap: true,
+    rollupOptions: {
+      input: path.resolve(__dirname, 'index.html'),
+      output: {
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash][extname]',
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+})
