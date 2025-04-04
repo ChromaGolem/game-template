ec50351262d64f90c755966497312b25d854b87e
Andrew Brown <drusepth@gmail.com>
2025-04-04 12:40:37 -0700

    add 0.0.0.0 binding to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import path from 'path'
 
 export default defineConfig({
+  server: {
+    host: '0.0.0.0',
+  },
   base: './', // Use relative paths (important if you're deploying somewhere like file:// or subdirectories)
   build: {
     outDir: 'dist',
